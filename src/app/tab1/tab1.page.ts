a46cf82c849dd57f556781d091be66d0ed0f75d8
jaime <pepefloress.1999@gmail.com>
2023-09-18 09:43:00 -0600

    prueba

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,29 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss']
 })
-export class Tab1Page {
+export class Tab1Page  implements OnInit{
 
   constructor() {}
+
+  ngOnInit(): void {
+
+    let numero = 10;
+
+    // debugger;
+
+    let total = numero + 15;
+
+    total += 15;
+
+
+
+
+
+    console.log(total)
+  }
 
 }
